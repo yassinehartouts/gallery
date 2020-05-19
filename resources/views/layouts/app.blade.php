d4cae3c76ed2fe701988e714e40c4dc9e3a00bcf
elhartiyassine <yassineman7ouse@gmail.com>
2020-05-19 12:25:25 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <script data-ad-client="ca-pub-6074738390091469" 
-    async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script data-ad-client="ca-pub-6074738390091469"
+     async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
     
     <meta charset="utf-8">
     <title>PhotoShow</title>
